61ec0510dea7c4a647102e12c1ed850fc4320adb
arif98741 <arifsofg@gmail.com>
2018-02-08 21:49:43 +0600

    Code 15.2 Added

---
+++ Code Backup/Tutorial 15.2/index.php
@@ -0,0 +1,21 @@
+
+	<?php
+		 //Array Functions - Tutorial 15.2 
+		$a = array(
+			34,23,4,4,34,4,98
+		);
+	
+		echo array_sum($a);
+		echo "<pre>";
+		print_r($a);
+		echo "<pre>";
+		
+		
+		
+		echo "<pre>";
+		print_r(array_slice($a,4,1));
+		echo "<pre>";
+		
+		
+	?>
+	
--- index.php
+++ index.php
@@ -62,25 +62,25 @@
 			<p>Learn and Enjoy</p>
 		</div>
 		<div class="content">
-			<h2> Array Sorting - Tutorial 15.1 </h2>
+			<h2> Array Functions - Tutorial 15.2 </h2>
 			
 			<p>
 				<?php
-					$arr = array(4,34,35,32,8,34,83);
-		/*$arr = array(
-			"a" =>34,
-			"d" =>78,
-			"c" =>14,
-			"b" =>35,
-		);*/
-		sort($a);
-		rsort($a);
-		//ksort($arr);
-		//krsort($a);
-		foreach($arr as $r)
-		{
-			echo $r."</br>";
-		}
+					
+					$a = array(
+						34,23,4,4,34,4,98
+					);
+				
+					echo array_sum($a);
+					echo "<pre>";
+					print_r($a);
+					echo "<pre>";
+					
+					
+					
+					echo "<pre>";
+					print_r(array_slice($a,4,1));
+					echo "<pre>";
 					
 					
 				?>
